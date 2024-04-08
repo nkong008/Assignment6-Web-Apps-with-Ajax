5d71e55886f6f3aa2d5bf0eb6aa338af45174f8c
nkong008 <nkong2020@gmail.com>
2024-04-07 20:26:07 -0500

    now displays

--- xhr-request/js/script.js
+++ xhr-request/js/script.js
@@ -125,19 +125,42 @@
         }
     }
 
+
+    // let anchor = document.createElement("a");
+    let xHR = document.getElementById('xhrDataDisplay');
+
     // handle XHR success
     function updateUISuccess(responseText) {
         var response = JSON.parse(responseText);
-        // var condition = response.artworks[0].main;
+        // var condition = response.data[0].api_link;
+        
+        for(let i = 0; i < response.data.length; i++) {
+
+            let output = '<section id="api-links">'
+            var condition = response.data[i].api_link;
+            let thumbnail = response.data[i].thumbnail.lqip;
+            output += '<a href = "' + condition + '" target ="_blank">' + condition + '</a>';
+            output += '<img src = "' + thumbnail + '" class ="thumbnail">';
+            // output += anchor;
+            // anchor.setAttribute('href', condition);
+            // anchor.setAttribute('target', '_blank');
+            // xHR.innerHTML = anchor;
+
+            console.log(condition);
+
+            output += "</section>"
+            $("#xhrDataDisplay").html(output); 
+        };
+
             
 
 
 
-
-        // need help with variables inorder to target them and display them onto the browser
+        // need help with variables in order to target them and display them onto the browser
 
 
         console.log(response);
+        // console.log(condition);
         // Perform actions with the response data as needed
     }
 
@@ -146,6 +169,6 @@
         console.log('Error making the request. Status:', status);
         
     }
-
+//jquery ends here
 })();
 
